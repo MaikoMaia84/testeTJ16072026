b92aa3966830363af5a242359bf9395c5101f682
maia <emaildomaiko@gmail.com>
2026-07-16 16:43:15 -0300

    ajustes testes

--- e2e/LoginP2.spec.js
+++ e2e/LoginP2.spec.js
@@ -2,18 +2,18 @@ const { test }  = require ('@playwright/test');
 const {abrirSiteQA, logarSucessoQA, verificarSeLogadoQA, logarEmailVazioQA, mensagemEmailVazioQA, cadastroQA } = require ('./loginPage')
 
 
-test('Login com sucesso 2 ', async ({ page }) => {
+test('Login com sucesso  ', async ({ page }) => {
     await abrirSiteQA(page)
     await logarSucessoQA(page)
     await verificarSeLogadoQA(page)
  })
 
-test('Login email vazio e mansagem ', async ({ page }) => {
+test('Login email vazio e mansage ', async ({ page }) => {
     await abrirSiteQA(page)
     await logarEmailVazioQA (page)
     await mensagemEmailVazioQA (page)
  })
-test('cadastro QA', async ({ page }) => {
+test('cadastro', async ({ page }) => {
     await abrirSiteQA(page)
     await cadastroQA(page)
  });
